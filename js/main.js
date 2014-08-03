dbc3513f32c913ab8d67275ffae735d4b46be641
Dwight Spencer <denzuko@users.sf.net>
2014-08-03 05:20:06 +0000

    prepping for anglularjs conversion

--- js/main.js
+++ js/main.js
@@ -13,17 +13,21 @@ var libs = [
     "escapes"
 ];
 
-require(libs, function($) {
-
+require(libs, function($,escapes) {
   $(document).ready(function() {
     escapes('/screen.txt', function () {
       $(this).appendTo('#art');
       $("#art").fadeIn(500);
     });
 
-    setTimeout(function(){var a=document.createElement("script");
-var b=document.getElementsByTagName("script")[0];
-a.src=document.location.protocol+"//dnn506yrbagrg.cloudfront.net/pages/scripts/0009/1652.js?"+Math.floor(new Date().getTime()/3600000);
-a.async=true;a.type="text/javascript";b.parentNode.insertBefore(a,b)}, 1);
+    setTimeout(function(){
+      var uri="//dnn506yrbagrg.cloudfront.net/pages/scripts/0009/1652.js?";
+      var t=3600000;
+      var proto=document.location.protocol;
+      var a=document.createElement("script");
+      var b=document.getElementsByTagName("script")[0];
+          a.src=proto+uri+Math.floor(new Date().getTime()/5);
+          a.async=true;
+          a.type="text/javascript";b.parentNode.insertBefore(a,b)}, 1);
   });
 });
